d342be6540acd377e72de7da0ad95d2fe01b4b6b
MehemmedIsmayilov <mehemmedmi@code.edu.az>
2023-02-27 15:37:30 +0400

    period 2

--- main.js
+++ main.js
@@ -14,14 +14,27 @@ function addList() {
   let input = document.querySelector(".input").value;
   let tbody = document.querySelector(".tbody");
 
-  let newList = new list(input, button, delet);
+  let newList = new list(input);
   listArr.push(newList);
   listArr.map((list_get) => {
     tbody.innerHTML += `
-<li>${list_get.input}</li>
-<button class="delete">DELETE</button>
+<li>${list_get.input}</li>  
+
 `;
   });
 }
 
 button.addEventListener("click", addList);
+
+
+
+delet.addEventListener("click",function(){
+  const indexToDelete=2;
+  listArr.splice(indexToDelete,1);
+})
+// delet.addEventListener("click",()=>{
+//   let newListt=new list(input)
+//   listArr.pop(newListt)
+// })
+
+// delet.addEventListener("click",delete listArr[1])
